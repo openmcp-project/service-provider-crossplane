3149e4ab92a5c6722c7768bf808a5c3ea88d162f
Radek Schekalla (SAP) <radek.schekalla@sap.com>
2025-11-25 08:34:17 +0100

    task generate
    On-behalf-of: Radek Schekalla (SAP) <radek.schekalla@sap.com>
    Signed-off-by: Radek Schekalla (SAP) <radek.schekalla@sap.com>

--- pkg/component/platform_secret_component.go
+++ pkg/component/platform_secret_component.go
@@ -5,10 +5,11 @@ import (
 
 	"github.com/openmcp-project/control-plane-operator/pkg/juggler"
 	"github.com/openmcp-project/control-plane-operator/pkg/juggler/object"
-	"github.com/openmcp-project/service-provider-crossplane/pkg/utils"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/openmcp-project/service-provider-crossplane/pkg/utils"
 )
 
 var _ object.ObjectComponent = &PlatformSecret{}
